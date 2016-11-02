8e2f1b837e87a1922ef91902e60b9916a3287c38
jiangkui <1149292216@qq.com>
2016-11-02 11:45:56 +0800

    修改

--- library/src/main/java/com/mph/library/progress/ProgressCancelListener.java
+++
@@ -1,9 +0,0 @@
-package com.mph.library.progress;
-
-/**
- * Created by：hcs on 2016/10/17 16:40
- * e_mail：aaron1539@163.com
- */
-public interface ProgressCancelListener {
-    void onCancelProgress();
-}
